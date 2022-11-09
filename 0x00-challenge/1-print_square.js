b29c1c796b7a7ff790da81731b25f9005cfa48c7
Eninetjanice <janetheni@gmail.com>
2022-11-09 04:08:14 +0000

    Print square

--- 0x00-challenge/1-print_square.js
+++ 0x00-challenge/1-print_square.js
@@ -18,9 +18,8 @@ size = parseInt(process.argv[2], 16)
 
 for (let i = 0 ; i <= size ; i ++) {
     for (let j = 0 ; j <= size ; j ++) {
-        if (i == 0 || i == size || b == 0 || b == size)
-            process.stdout.write("#");
-        }
+        process.stdout.write("#");
+    }
     process.stdout.write("\n");
 }
 
